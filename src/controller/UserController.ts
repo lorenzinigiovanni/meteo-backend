e624c6888eea1daf316e5da758cea26405294d3a
Simone Luchetta <simo_lucketts@outlook.it>
2020-10-06 18:39:31 +0200

    Create route to associate weather stations to users

--- src/controller/UserController.ts
+++ src/controller/UserController.ts
@@ -3,6 +3,7 @@ import { BaseRequest } from './BaseRequest';
 import { JSONSchema } from '../utility/decorators/JSONSchema';
 
 import { User } from '../entity/User';
+import { WeatherStation } from '../entity/WeatherStation';
 
 export class UserController {
 
@@ -15,12 +16,29 @@ export class UserController {
                 const data = UserPost.fromJSON(req.body);
 
                 const user = new User();
+
                 user.name = data.name;
                 user.surname = data.surname;
                 user.email = data.email;
                 user.password = data.password;
 
                 res.status(201).send([await user.save()]);
+            })
+        app.route('/users/:ID/weatherstation')
+            .post(async (req, res) => {
+                const data = UserWeatherStationPost.fromJSON(req.body);
+                const params = req.params;
+
+                const user = await User.findOne({ where: { ID: params['ID'] }, relations: ["weatherStations"] });
+                const weatherStations = await WeatherStation.findByIds(data.weatherStationsID);
+
+                if (user !== undefined) {
+                    user.weatherStations.push(...weatherStations);
+
+                    await user.save();
+                }
+
+                res.status(201).send();
             })
     }
 }
@@ -31,4 +49,9 @@ export class UserPost extends BaseRequest {
     name!: string;
     email!: string;
     password!: string;
+}
+
+@JSONSchema()
+export class UserWeatherStationPost extends BaseRequest {
+    weatherStationsID!: string[];
 }
